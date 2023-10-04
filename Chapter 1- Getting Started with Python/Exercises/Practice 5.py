79f8d6071951d4d170eaa314ac777953a739b2ed
Eillmaa <nazninjamshedali@gmail.com>
2023-10-04 14:08:52 +0400

    Practice 5

---
+++ Chapter 1- Getting Started with Python/Exercises/Practice 5.py
@@ -0,0 +1,8 @@
+#Write 4 strings in different variables and print the output as one string
+
+name = "naz"
+age = "17"
+uni = "bsu"
+location = "rak"
+
+print (name + " is " + age + ", she goes to " + uni + " in " + location)
