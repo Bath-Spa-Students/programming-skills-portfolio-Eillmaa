07403be09f7479d15feea9f6195ccf140effa528
Eillmaa <nazninjamshedali@gmail.com>
2023-10-11 10:16:28 +0400

    Exercise 1.py

--- Chapter 1- Getting Started with Python/Exercises/Exercise 1.py
+++ Chapter 1- Getting Started with Python/Exercises/Exercise 1.py
@@ -1,6 +1,6 @@
-print ("""Twinkle, twinkle, little star,
-    How I wonder what you are!
-         Up above the world so high,
-         Like a diamond in the sky.
-Twinkle, twinkle, little star,
-     How I wonder what you are""")
+print ('Twinkle, twinkle, little star,',end='\n')
+print ('    How I wonder what you are!',end='\n')
+print ('         Up above the world so high,',end='\n')
+print ('         Like a diamond in the sky.',end='\n')
+print ('Twinkle, twinkle, little star,',end='\n')
+print ('    How I wonder what you are')
