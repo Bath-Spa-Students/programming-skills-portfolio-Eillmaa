51f9051b7e1985d434fb141f30e806c7ccf18ca7
Eillmaa <nazninjamshedali@gmail.com>
2023-10-18 10:37:33 +0400

    Exercise 5.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise 5.py
@@ -0,0 +1,8 @@
+#USB Shopper
+
+her_amount = 50
+price = 6
+able_to_buy = (her_amount/price)
+print ('Can buy:',int(able_to_buy),'USB sticks')
+amount_left1 = (her_amount-price*able_to_buy)
+print ('Pounds remianing: £',int(amount_left1))
