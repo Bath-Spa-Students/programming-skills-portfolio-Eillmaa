62205418ccfdce08836ea35a559d02dc43723ebb
Eillmaa <nazninjamshedali@gmail.com>
2023-10-18 20:15:59 +0400

    Exercise 4.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise 4.py
@@ -0,0 +1,7 @@
+#Favorite number
+#Use a variable to represent your favorite number
+favnum = '23'
+#Create a message that reveals your favorite nuber using the variable
+msg = 'My favorite number is ' + favnum
+#Print the message
+print (msg)
