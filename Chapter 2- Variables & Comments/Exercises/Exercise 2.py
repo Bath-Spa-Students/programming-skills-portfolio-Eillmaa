9ee9ab96e32c627f9c8fb7d1b46d0ed40fee62ac
Eillmaa <nazninjamshedali@gmail.com>
2023-10-18 20:23:40 +0400

    Exercise 2.py

--- Chapter 2- Variables & Comments/Exercises/Exercise 2.py
+++ Chapter 2- Variables & Comments/Exercises/Exercise 2.py
@@ -1,2 +1,6 @@
+#Variables
+#Author name
 author_name = ('Ralph Waldo Emerson')
+
+#Print the qoute with author name and 'once said,'
 print (author_name,'once said,','"Do not go where the path may lead, go instead where there is no path and leave a trail.')
