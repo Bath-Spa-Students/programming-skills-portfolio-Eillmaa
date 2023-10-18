ec53298681c4ad25331990597dcefea5bf360f63
Eillmaa <nazninjamshedali@gmail.com>
2023-10-18 20:35:44 +0400

    Exercise 3.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise 3.py
@@ -0,0 +1,13 @@
+#Stripping names
+#Assign a name to a variable and add space before and after the name and print it once
+name = '\tXiao\n'
+print (name)
+
+#Print name using lstrip()
+print (name.lstrip())
+
+#Print name using rstrip()
+print (name.rstrip())
+
+#Print name using strip()
+print (name.strip())
