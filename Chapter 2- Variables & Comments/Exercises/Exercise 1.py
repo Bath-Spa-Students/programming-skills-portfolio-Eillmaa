e1418780fe474cafa452aa5c8e46b4502ba86794
Eillmaa <nazninjamshedali@gmail.com>
2023-10-18 20:21:04 +0400

    Exercise 1.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise 1.py
@@ -0,0 +1,8 @@
+#Variables
+#Assign message to variable and print it
+a = ('Hello! today is such a lovely day')
+print (a)
+
+#Change the value of the variable to a new message and print
+a = ('It is a good day to take a walk')
+print (a)
