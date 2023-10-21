5d65bd94dd4931f63aa09ba66a991158178a40df
Eillmaa <nazninjamshedali@gmail.com>
2023-10-21 22:12:29 +0400

    Exercise 2.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise 2.py
@@ -0,0 +1,10 @@
+#Greetings
+#List and message
+names = ('aysa','yusra','alisha','fayha')
+message = ('Hi!, I miss you ')
+
+#Print message with each name
+print ((message) + (names[0]))
+print ((message) + (names[1]))
+print ((message) + (names[2]))
+print ((message) + (names[3]))
