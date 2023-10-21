073c028a24c22bce214e429319874fc2912d5662
Eillmaa <nazninjamshedali@gmail.com>
2023-10-21 22:12:51 +0400

    Exercise 4.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise 4.py
@@ -0,0 +1,9 @@
+#Guest List
+#List of people to invite to dinner
+invited = ('xiao','neuvi','haitham','ayato')
+
+#Print a message inviting them using the list
+print ('Hey ' + (invited[0]) + '!, you are invited to dinner')
+print ('Hey ' + (invited[1]) + '!, you are invited to dinner')
+print ('Hey ' + (invited[2]) + '!, you are invited to dinner')
+print ('Hey ' + (invited[3]) + '!, you are invited to dinner')
