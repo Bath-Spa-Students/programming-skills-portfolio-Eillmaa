00488299c940bea92a8a04ca0379e8433f89c0ce
Eillmaa <nazninjamshedali@gmail.com>
2023-10-21 22:12:16 +0400

    Exercise 1.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise 1.py
@@ -0,0 +1,9 @@
+#Names
+#Store a few friends names in a list calles names
+names = ('aysa','yusra','alisha','fayha')
+
+#Print the names one at a time
+print (names[0])
+print (names[1])
+print (names[2])
+print (names[3])
