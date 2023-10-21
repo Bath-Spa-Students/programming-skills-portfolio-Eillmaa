81ca71282258b706d5f1ee5a38add1238acf7fc0
Eillmaa <nazninjamshedali@gmail.com>
2023-10-21 22:12:40 +0400

    Exercise 3.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise 3.py
@@ -0,0 +1,9 @@
+#Your Own List
+#List
+transportation = ('car','bus','bicycle','motorcycle')
+
+#Print statements about these items
+print ('I want to own a black ' + (transportation[0]) + ' in the future')
+print ('I go to my university by ' + (transportation[1]))
+print ('I used to ride a ' + (transportation[2]) + ' a lot when I was younger')
+print ('I think ' + (transportation[3]) + ' owners are really cool')
