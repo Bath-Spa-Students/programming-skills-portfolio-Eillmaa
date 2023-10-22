56301703e2b02e624012e4c66307c694f2cced16
Eillmaa <nazninjamshedali@gmail.com>
2023-10-22 17:27:29 +0400

    Exercise 7.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise 7.py
@@ -0,0 +1,24 @@
+#Seeing the World
+
+locations = ['Japan','Qatar','Canada','Saudi Arabia','London']
+print(locations)
+
+print (sorted(locations))
+
+print (locations)
+
+print (sorted(locations,reverse=True))
+
+print (locations)
+
+locations.reverse()
+print (locations)
+
+locations.reverse()
+print (locations)
+
+locations.sort()
+print (locations)
+
+locations.sort(reverse=True)
+print (locations)
