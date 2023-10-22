839c75dc894de99dcec69df2f36f72c0c057fb71
Eillmaa <nazninjamshedali@gmail.com>
2023-10-22 16:59:34 +0400

    Exercise 5.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise 5.py
@@ -0,0 +1,19 @@
+#Change Guest List
+
+invited = ['xiao','neuvi','haitham','ayato']
+print ('Hey ' + (invited[0]) + '!, you are invited to dinner')
+print ('Hey ' + (invited[1]) + '!, you are invited to dinner')
+print ('Hey ' + (invited[2]) + '!, you are invited to dinner')
+print ('Hey ' + (invited[3]) + '!, you are invited to dinner'+ '\n')
+
+#Print the guest that can't make it
+print ((invited[3]) + ' can not make it to dinner' + '\n')
+
+#Modify the list
+invited[3]='scara'
+
+#Print a new set of invitations
+print ('Hey ' + (invited[0]) + '!, you are invited to dinner')
+print ('Hey ' + (invited[1]) + '!, you are invited to dinner')
+print ('Hey ' + (invited[2]) + '!, you are invited to dinner')
+print ('Hey ' + (invited[3]) + '!, you are invited to dinner')
