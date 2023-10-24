ad9813802f6b72f64d96d16c6e7d5c37e85f64ac
Eillmaa <nazninjamshedali@gmail.com>
2023-10-24 09:40:18 +0400

    Exercise 7.py

--- Chapter 3- Data Structures/Exercises/Exercise 7.py
+++ Chapter 3- Data Structures/Exercises/Exercise 7.py
@@ -1,24 +1,32 @@
 #Seeing the World
-
+#Store 5 locations in a list and print it
 locations = ['Japan','Qatar','Canada','Saudi Arabia','London']
 print(locations)
 
+#Use sorted() to print your list in alphabetical order
 print (sorted(locations))
 
+#Show that your list is still in its original order
 print (locations)
 
+#Use sorted() to print your list in reverse alphabetical order
 print (sorted(locations,reverse=True))
 
+#Show that your list is still in its original order
 print (locations)
 
+#Use reverse() to change the order of your list
 locations.reverse()
 print (locations)
 
+#Use reverse() to change the order of your list again
 locations.reverse()
 print (locations)
 
+#Use sort() to change your list so it’s stored in alphabetical order
 locations.sort()
 print (locations)
 
+#Use sort() to change your list so it’s stored in reverse alphabetical order
 locations.sort(reverse=True)
 print (locations)
