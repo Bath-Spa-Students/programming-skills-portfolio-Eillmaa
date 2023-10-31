2a9854fb8e64037814b04cd0a8a68543fcd9eb86
Eillmaa <nazninjamshedali@gmail.com>
2023-10-31 22:02:29 +0400

    Exercise 3

---
+++ Chapter 4- Control Flow/Exercises/Exercise 3.py
@@ -0,0 +1,33 @@
+#Alien colors #3
+#Assigning variable
+alien_color = 'green'
+
+#Start with if function
+if alien_color == 'green':
+    print ('You earned 5 points!')
+
+#Elif function in case alien color is not green
+elif alien_color == 'yellow':
+    print ('You earned 10 points!')
+
+#Else function used incase alien color is not green and yellow
+else:
+    print ('You earned 15 points!')
+
+#Version of the program for yellow alien message
+alien_color = 'yellow'
+if alien_color == 'green':
+    print ('You earned 5 points!')
+elif alien_color == 'yellow':
+    print ('You earned 10 points!')
+else:
+    print ('You earned 15 points!')
+
+#Version of program for red alien message
+alien_color = 'red'
+if alien_color == 'green':
+    print ('You earned 5 points!')
+elif alien_color == 'yellow':
+    print ('You earned 10 points!')
+else:
+    print ('You earned 15 points!')
