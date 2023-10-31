21176a21513ae01575105e9971f01df9ca9bd412
Eillmaa <nazninjamshedali@gmail.com>
2023-10-31 22:02:48 +0400

    Exercise 4

---
+++ Chapter 4- Control Flow/Exercises/Exercise 4.py
@@ -0,0 +1,19 @@
+#Stages of life
+#Assigning age variable
+age = 18
+
+#If statement
+if age<2:
+    print('Person is a baby')
+elif age>=2 and age<4:
+    print('Person is a toddler')
+elif age>=4 and age<13:
+    print('Person is a kid')
+elif age>=13 and age<20:
+    print('Person is a teenager')
+elif age>=20 and age<65:
+    print('Person is an adult')
+
+#Else for ages other than the one in the program
+else:
+    print('Person is an elder')
