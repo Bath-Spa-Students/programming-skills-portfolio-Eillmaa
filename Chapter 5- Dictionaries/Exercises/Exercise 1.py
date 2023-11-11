1338d052cb17453fb7765557f864df76ea3ec495
Eillmaa <nazninjamshedali@gmail.com>
2023-11-11 21:28:29 +0400

    Exercise 1

---
+++ Chapter 5- Dictionaries/Exercises/Exercise 1.py
@@ -0,0 +1,11 @@
+#Person
+#Make a dictionary containing first name, last name, age and city, then print it
+friend = {'first_name':'Aysa',
+              'last_name':'Akhter',
+              'age':'18',
+              'city':'Ajman'}
+
+print (friend['first_name'])
+print (friend['last_name'])
+print (friend['age'])
+print (friend['city'])
