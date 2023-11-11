ee34d39cf85923d1d04b38f0f7cf2c7719b99300
Eillmaa <nazninjamshedali@gmail.com>
2023-11-11 22:48:54 +0400

    Exercise 5

---
+++ Chapter 4- Control Flow/Exercises/Exercise 5.py
@@ -0,0 +1,18 @@
+#Favorite Fruit
+
+favorite_fruits = ['oranges','bananas','pineapples']
+
+if 'oranges' in favorite_fruits:
+    print('I really like '+favorite_fruits[0]+'.')
+
+if 'bananas' in favorite_fruits:
+    print('I really like '+favorite_fruits[1]+'.')
+
+if 'pineapples' in favorite_fruits:
+    print('I really like '+favorite_fruits[2]+'.')
+
+if 'oranges' in favorite_fruits:
+    print('I like drinking '+favorite_fruits[0]+' juice.')
+
+if 'bananas' in favorite_fruits:
+    print('I like drinking '+favorite_fruits[0]+' milk juice.')
