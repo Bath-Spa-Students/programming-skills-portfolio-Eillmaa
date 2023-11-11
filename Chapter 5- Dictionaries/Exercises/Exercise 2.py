cfe97bf8deadb34c3cb16736395d50d3dd305da7
Eillmaa <nazninjamshedali@gmail.com>
2023-11-11 21:28:39 +0400

    Exercise 2

---
+++ Chapter 5- Dictionaries/Exercises/Exercise 2.py
@@ -0,0 +1,18 @@
+#Glossary
+
+glossary = {'Variable':'A name that represents value stored in the computer memory.',
+              'List':'An object that contains multiple data items',
+              'Dictionary':'An object that stores a collection of data',
+              'Print':'A function that shows the result of the code written',
+              'Input':'A built in function that gets input from the user by showing a promt'}
+
+x = 'Variable'
+print (x+': '+glossary['Variable'])
+x = 'List'
+print ('\n'+x+': '+glossary['List'])
+x = 'Dictionary'
+print ('\n'+x+': '+glossary['Dictionary'])
+x = 'Print'
+print ('\n'+x+': '+glossary['Print'])
+x = 'Input'
+print ('\n'+x+': '+glossary['Input'])
