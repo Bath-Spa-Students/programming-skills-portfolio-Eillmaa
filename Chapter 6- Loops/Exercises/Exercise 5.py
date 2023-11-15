4bd885bdc4c459755df7ccb25520c2eecdebaa95
Eillmaa <nazninjamshedali@gmail.com>
2023-11-15 09:51:16 +0400

    Exercise 5

--- Chapter 6- Loops/Exercises/Exercise 5.py
+++ Chapter 6- Loops/Exercises/Exercise 5.py
@@ -1,6 +1,8 @@
 #No Pastrami
 print('Deli has run out of pastrami sandwich')
-sandwich_orders=['tikka sandwich','pastrami sandwich','vegetable sandwich','pastrami sandwich','egg sandwich','pastrami sandwich','chilli sandwich']
+sandwich_orders=['tikka sandwich','pastrami sandwich',
+                 'vegetable sandwich','pastrami sandwich','egg sandwich',
+                 'pastrami sandwich','chilli sandwich']
 finished_sandwiches=[]
 
 while 'pastrami sandwich' in sandwich_orders:
