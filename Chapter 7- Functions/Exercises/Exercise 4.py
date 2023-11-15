2fdfc4759bea29390b9cc2a3a0e45ba12b5af300
Eillmaa <nazninjamshedali@gmail.com>
2023-11-15 20:30:01 +0400

    Exercise 4

---
+++ Chapter 7- Functions/Exercises/Exercise 4.py
@@ -0,0 +1,12 @@
+#Large Shirts
+
+def make_shirt(size='large',message='I love Python'):
+    print('The size of the shirt is '+size)
+    print('The message printed on the shirt is: '+message)
+
+#Large shirt
+make_shirt()
+#Medium shirt
+make_shirt(size='medium')
+#Different message
+make_shirt(message='Coding is fun')
