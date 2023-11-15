4617947d9250ffe25976b575056e113761834863
Eillmaa <nazninjamshedali@gmail.com>
2023-11-15 20:29:28 +0400

    Exercise 1

---
+++ Chapter 7- Functions/Exercises/Exercise 1.py
@@ -0,0 +1,7 @@
+#Message
+#Function that prints what you are learning in this chapter
+def display_message():
+    print('We are going to learn about functions')
+
+#Calling the function
+display_message()
