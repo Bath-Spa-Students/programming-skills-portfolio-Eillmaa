f5a8e5d48c4c867cac6f34f878ca3f04e28adcb1
Eillmaa <nazninjamshedali@gmail.com>
2023-11-15 21:41:11 +0400

    Practice 3

---
+++ Chapter 2- Variables & Comments/Exercises/Practice 3.py
@@ -0,0 +1,13 @@
+v=input()
+w=input()
+x=input()
+y=input()
+z=input()
+
+v_type=(str(v))
+w_type=(int(w))
+x_type=(float(x))
+y_type=(int(y))
+z_type=(float(z))
+
+print (v_type,w_type,x_type,y_type,z_type)
