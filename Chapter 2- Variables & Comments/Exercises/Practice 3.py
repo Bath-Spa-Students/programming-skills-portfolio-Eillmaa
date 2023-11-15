2e2afb1bfa2437125f4cff29d9df2c728b8c3b11
Eillmaa <nazninjamshedali@gmail.com>
2023-11-15 22:29:19 +0400

    Practice 3

--- Chapter 2- Variables & Comments/Exercises/Practice 3.py
+++ Chapter 2- Variables & Comments/Exercises/Practice 3.py
@@ -1,13 +1,16 @@
+#Take 5 inputs from user
 v=input()
 w=input()
 x=input()
 y=input()
 z=input()
 
+#Type cast those outputs
 v_type=(str(v))
 w_type=(int(w))
 x_type=(float(x))
 y_type=(int(y))
 z_type=(float(z))
 
+#Print them
 print (v_type,w_type,x_type,y_type,z_type)
