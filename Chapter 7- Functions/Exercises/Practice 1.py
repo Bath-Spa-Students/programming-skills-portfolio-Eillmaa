5789af47608323cb5da02e86fe2732245b11812b
Eillmaa <nazninjamshedali@gmail.com>
2023-11-16 10:52:39 +0400

    Practice 1

--- Chapter 7- Functions/Exercises/Practice 1.py
+++ Chapter 7- Functions/Exercises/Practice 1.py
@@ -1,4 +1,5 @@
-#Write a Python function that takes two numbers as arguments and returns their sum
+#Write a Python function that takes two numbers
+#as arguments and returns their sum
 
 def num(num1,num2):
     print (num1+num2)
